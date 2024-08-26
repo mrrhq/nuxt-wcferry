c06e95694d32174ef27446938f73ad91caa23de2
kejun <kejun1997@gmail.com>
2024-08-27 06:56:11 +0800

    chore: fix runtime config key

--- src/module.ts
+++ src/module.ts
@@ -13,7 +13,7 @@ export default defineNuxtModule<ModuleOptions>({
   setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
     addServerScanDir(resolver.resolve('./runtime/server'))
-    nuxt.options.runtimeConfig['ferry'] = options
+    nuxt.options.runtimeConfig['wcferry'] = options
     nuxt.options.nitro ??= {}
     nuxt.options.nitro.experimental ??= {}
     nuxt.options.nitro.experimental.tasks = true
